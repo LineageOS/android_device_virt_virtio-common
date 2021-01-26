ae537871830897979df866cc2b33335226a0dd91
Peter Collingbourne <pcc@google.com>
2021-01-25 22:15:20 -0800

    [DNM] virtio-common: Declare support for wifi.
    Context.getSystemService("wifi") now throws an exception if the device
    does not declare support for wifi. Moreover, even if I change the
    implementation to return null instead of throwing, there are a number
    of applications that fail to run correctly if the method returns null,
    including Settings.
    
    It seems like the rest of the system is pretty much assuming wifi
    support at this point. So let's just pretend that we support wifi by
    including android.hardware.wifi.xml in our vendor partition.
    
    Bug: 142352330
    Change-Id: I702b127fc1ec49e327f2a7349a954c1185460459

--- device-common.mk
+++ device-common.mk
@@ -150,6 +150,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.credentials.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.credentials.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
