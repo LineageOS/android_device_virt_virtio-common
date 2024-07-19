c8238017d1b74fd26fc80c42bd787172ef3aeec1
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-19 17:02:02 +0800

    virtio-common: libinit: Import from xiaomi sm8250-common
    Change-Id: I161a159fa418356a6c15aa31dc7544ff7c150952

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,6 +28,10 @@ else
 include device/google/cuttlefish/shared/virgl/BoardConfig.mk
 endif
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_kona
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_kona
+
 # Kernel
 BOARD_KERNEL_CMDLINE := \
     loop.max_part=7 \
