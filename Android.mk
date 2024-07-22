d7da5870ec868799ecf832fc3e7c80af212b382c
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-22 22:31:25 +0800

    virtio-common: Add /vendor/firmware_mnt to fstab and firmware search path
    * When we passthrough some host devices (such as GPU) to the VM,
      and if it requires firmware to function, we have to put firmware
      inside the VM.
    * It'll be a hassle to include all the firmwares in vendor partition
      and maintain it. Let the users put whatever firmware they need into
      a separate partition.
    
    Change-Id: I0c4e69454961695e288b300b5f624ccc16c832e6

--- Android.mk
+++ Android.mk
@@ -8,4 +8,12 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(USES_DEVICE_VIRT_COMMON),true)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
+$(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT)
+
 endif
