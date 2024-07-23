b54a70d815371d9419e9a673e467152e6dfa8118
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-23 23:19:10 +0800

    virtio-common: grub: Initial theme support
    Change-Id: I10ba9d49cdde6ccab1ee29d01a4e46734fae7afc

--- grub/Android.mk
+++ grub/Android.mk
@@ -20,10 +20,12 @@ GRUB_WORKDIR_BOOT := $(GRUB_WORKDIR_BASE)/boot
 ifneq ($(LINEAGE_BUILD),)
 GRUB_ANDROID_DISTRIBUTION_NAME := LineageOS $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
 GRUB_ARTIFACT_FILENAME_PREFIX := lineage-$(LINEAGE_VERSION)
+GRUB_THEME := lineage
 else
 LOCAL_BUILD_DATE := $(shell date -u +%Y%m%d)
 GRUB_ANDROID_DISTRIBUTION_NAME := Android $(PLATFORM_VERSION_LAST_STABLE) $(BUILD_ID)
 GRUB_ARTIFACT_FILENAME_PREFIX := Android-$(PLATFORM_VERSION_LAST_STABLE)-$(BUILD_ID)-$(LOCAL_BUILD_DATE)
+GRUB_THEME := android
 endif
 
 # $(1): output file
@@ -34,6 +36,11 @@ define make-isoimage-boot-target
 	cp $(COMMON_GRUB_PATH)/grub-boot.cfg $(GRUB_WORKDIR_BOOT)/boot/grub/grub.cfg
 	sed -i "s|@GRUB_ANDROID_DISTRIBUTION_NAME@|$(GRUB_ANDROID_DISTRIBUTION_NAME)|g" $(GRUB_WORKDIR_BOOT)/boot/grub/grub.cfg
 	sed -i "s|@STRIPPED_TARGET_GRUB_KERNEL_CMDLINE@|$(strip $(TARGET_GRUB_KERNEL_CMDLINE))|g" $(GRUB_WORKDIR_BOOT)/boot/grub/grub.cfg
+
+	sed -i "s|@GRUB_THEME@|$(GRUB_THEME)|g" $(GRUB_WORKDIR_BOOT)/boot/grub/grub.cfg
+	rm -rf $(GRUB_WORKDIR_BOOT)/boot/grub/themes/$(GRUB_THEME)
+	$(if $(GRUB_THEME), cp -r $(COMMON_GRUB_PATH)/themes/$(GRUB_THEME) $(GRUB_WORKDIR_BOOT)/boot/grub/themes/)
+
 	$(GRUB_PATH_OVERRIDE) $(GRUB_PREBUILT_DIR)/bin/grub-mkrescue -d $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) --xorriso=$(GRUB_XORRISO_EXEC) -o $(1) $(2) $(GRUB_WORKDIR_BOOT)
 endef
 
