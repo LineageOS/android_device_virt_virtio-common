26e538a0cc732ae9b5ec1ce0eb7d65301c5cbd58
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-26 19:31:15 +0800

    virtio-common: Commonize some variables from grub/Android.mk
    Change-Id: Ifb42fe2a11099bc407e0643e4c97782240719451

--- grub/Android.mk
+++ grub/Android.mk
@@ -11,9 +11,6 @@ ifeq ($(TARGET_GRUB_ARCH),)
 $(warning TARGET_GRUB_ARCH is not defined, could not build GRUB)
 else
 GRUB_PREBUILT_DIR := prebuilts/grub/$(HOST_PREBUILT_TAG)/$(TARGET_GRUB_ARCH)
-GRUB_TOOLS_LINEAGE_BIN_DIR := prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin
-GRUB_PATH_OVERRIDE := PATH=$(GRUB_TOOLS_LINEAGE_BIN_DIR):$$PATH
-GRUB_XORRISO_EXEC := $(GRUB_TOOLS_LINEAGE_BIN_DIR)/xorriso
 
 GRUB_WORKDIR_BASE := $(TARGET_OUT_INTERMEDIATES)/GRUB_OBJ
 GRUB_WORKDIR_BOOT := $(GRUB_WORKDIR_BASE)/boot
@@ -25,37 +22,18 @@ GRUB_EFI_BOOT_FILENAME := BOOTX64.EFI
 GRUB_MKSTANDALONE_FORMAT := x86_64-efi
 endif
 
-ifneq ($(LINEAGE_BUILD),)
-GRUB_ANDROID_DISTRIBUTION_NAME := LineageOS $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)
-GRUB_ANDROID_OTA_PACKAGE_NAME := lineage-$(LINEAGE_VERSION).zip
-GRUB_ARTIFACT_FILENAME_PREFIX := lineage-$(LINEAGE_VERSION)
-GRUB_THEME := lineage
-else
-LOCAL_BUILD_DATE := $(shell date -u +%Y%m%d)
-GRUB_ANDROID_DISTRIBUTION_NAME := Android $(PLATFORM_VERSION_LAST_STABLE) $(BUILD_ID)
-GRUB_ANDROID_OTA_PACKAGE_NAME := $(TARGET_PRODUCT)-ota.zip
-GRUB_ARTIFACT_FILENAME_PREFIX := Android-$(PLATFORM_VERSION_LAST_STABLE)-$(BUILD_ID)-$(LOCAL_BUILD_DATE)
-GRUB_THEME := android
-endif
-
-ifeq ($(PRODUCT_IS_AUTOMOTIVE),true)
-GRUB_ANDROID_DISTRIBUTION_NAME += Car
-else ifeq ($(PRODUCT_IS_ATV),true)
-GRUB_ANDROID_DISTRIBUTION_NAME += TV
-endif
-
 # $(1): filesystem root directory
 # $(2): path to grub.cfg file
 define install-grub-theme
-	sed -i "s|@GRUB_THEME@|$(GRUB_THEME)|g" $(2)
+	sed -i "s|@BOOTMGR_THEME@|$(BOOTMGR_THEME)|g" $(2)
 	mkdir -p $(1)/boot/grub/themes
-	rm -rf $(1)/boot/grub/themes/$(GRUB_THEME)
-	$(if $(GRUB_THEME), cp -r $(COMMON_GRUB_PATH)/themes/$(GRUB_THEME) $(1)/boot/grub/themes/)
+	rm -rf $(1)/boot/grub/themes/$(BOOTMGR_THEME)
+	$(if $(BOOTMGR_THEME), cp -r $(COMMON_GRUB_PATH)/themes/$(BOOTMGR_THEME) $(1)/boot/grub/themes/)
 endef
 
 # $(1): path to grub.cfg file
 define process-grub-cfg
-	sed -i "s|@GRUB_ANDROID_DISTRIBUTION_NAME@|$(GRUB_ANDROID_DISTRIBUTION_NAME)|g" $(1)
+	sed -i "s|@BOOTMGR_ANDROID_DISTRIBUTION_NAME@|$(BOOTMGR_ANDROID_DISTRIBUTION_NAME)|g" $(1)
 	sed -i "s|@GRUB_EFI_BOOT_FILENAME@|$(GRUB_EFI_BOOT_FILENAME)|g" $(1)
 	sed -i "s|@STRIPPED_BOARD_KERNEL_CMDLINE_CONSOLE@|$(strip $(BOARD_KERNEL_CMDLINE_CONSOLE))|g" $(1)
 	sed -i "s|@STRIPPED_TARGET_GRUB_KERNEL_CMDLINE@|$(strip $(TARGET_GRUB_KERNEL_CMDLINE))|g" $(1)
@@ -71,7 +49,7 @@ define make-espimage-target
 
 	cp $(COMMON_GRUB_PATH)/grub-standalone.cfg $(GRUB_WORKDIR_ESP)/grub-standalone.cfg
 	$(call process-grub-cfg,$(GRUB_WORKDIR_ESP)/grub-standalone.cfg)
-	$(GRUB_PATH_OVERRIDE) $(GRUB_PREBUILT_DIR)/bin/grub-mkstandalone -d $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) --locales="" --fonts="" --format=$(GRUB_MKSTANDALONE_FORMAT) --output=$(GRUB_WORKDIR_ESP)/fsroot/EFI/BOOT/$(GRUB_EFI_BOOT_FILENAME) --modules="configfile disk fat part_gpt search" "boot/grub/grub.cfg=$(COMMON_GRUB_PATH)/grub-standalone.cfg"
+	$(BOOTMGR_PATH_OVERRIDE) $(GRUB_PREBUILT_DIR)/bin/grub-mkstandalone -d $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) --locales="" --fonts="" --format=$(GRUB_MKSTANDALONE_FORMAT) --output=$(GRUB_WORKDIR_ESP)/fsroot/EFI/BOOT/$(GRUB_EFI_BOOT_FILENAME) --modules="configfile disk fat part_gpt search" "boot/grub/grub.cfg=$(COMMON_GRUB_PATH)/grub-standalone.cfg"
 
 	cp -r $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) $(GRUB_WORKDIR_ESP)/fsroot/boot/grub/$(TARGET_GRUB_ARCH)
 	cp $(GRUB_PREBUILT_DIR)/share/grub/unicode.pf2 $(GRUB_WORKDIR_ESP)/fsroot/boot/grub/fonts/unicode.pf2
@@ -84,9 +62,9 @@ define make-espimage-target
 
 	/usr/bin/dd if=/dev/zero of=$(1) bs=1M count=128
 	/sbin/mkfs.vfat -F 32 $(1)
-	$(GRUB_TOOLS_LINEAGE_BIN_DIR)/mcopy -i $(1) -s $(GRUB_WORKDIR_ESP)/fsroot/EFI ::
-	$(GRUB_TOOLS_LINEAGE_BIN_DIR)/mcopy -i $(1) -s $(GRUB_WORKDIR_ESP)/fsroot/boot ::
-	$(GRUB_TOOLS_LINEAGE_BIN_DIR)/mcopy -i $(1) $(2) ::
+	$(BOOTMGR_TOOLS_LINEAGE_BIN_DIR)/mcopy -i $(1) -s $(GRUB_WORKDIR_ESP)/fsroot/EFI ::
+	$(BOOTMGR_TOOLS_LINEAGE_BIN_DIR)/mcopy -i $(1) -s $(GRUB_WORKDIR_ESP)/fsroot/boot ::
+	$(BOOTMGR_TOOLS_LINEAGE_BIN_DIR)/mcopy -i $(1) $(2) ::
 endef
 
 INSTALLED_ESPIMAGE_TARGET := $(GRUB_WORKDIR_ESP)/EFI.img
@@ -112,10 +90,10 @@ define make-isoimage-boot-target
 	cp $(COMMON_GRUB_PATH)/grub-boot.cfg $(GRUB_WORKDIR_BOOT)/boot/grub/grub.cfg
 	$(call process-grub-cfg,$(GRUB_WORKDIR_BOOT)/boot/grub/grub.cfg)
 	$(call install-grub-theme,$(GRUB_WORKDIR_BOOT),$(GRUB_WORKDIR_BOOT)/boot/grub/grub.cfg)
-	$(GRUB_PATH_OVERRIDE) $(GRUB_PREBUILT_DIR)/bin/grub-mkrescue -d $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) --xorriso=$(GRUB_XORRISO_EXEC) -o $(1) $(2) $(GRUB_WORKDIR_BOOT)
+	$(BOOTMGR_PATH_OVERRIDE) $(GRUB_PREBUILT_DIR)/bin/grub-mkrescue -d $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) --xorriso=$(BOOTMGR_XORRISO_EXEC) -o $(1) $(2) $(GRUB_WORKDIR_BOOT)
 endef
 
-INSTALLED_ISOIMAGE_BOOT_TARGET := $(PRODUCT_OUT)/$(GRUB_ARTIFACT_FILENAME_PREFIX)-boot.iso
+INSTALLED_ISOIMAGE_BOOT_TARGET := $(PRODUCT_OUT)/$(BOOTMGR_ARTIFACT_FILENAME_PREFIX)-boot.iso
 INSTALLED_ISOIMAGE_BOOT_TARGET_DEPS := $(PRODUCT_OUT)/kernel $(INSTALLED_COMBINED_RAMDISK_TARGET) $(INSTALLED_COMBINED_RAMDISK_RECOVERY_TARGET)
 $(INSTALLED_ISOIMAGE_BOOT_TARGET): $(INSTALLED_ISOIMAGE_BOOT_TARGET_DEPS)
 	$(call make-isoimage-boot-target,$(INSTALLED_ISOIMAGE_BOOT_TARGET),$(INSTALLED_ISOIMAGE_BOOT_TARGET_DEPS))
@@ -138,11 +116,11 @@ define make-isoimage-install-target
 	cp $(COMMON_GRUB_PATH)/grub-install.cfg $(GRUB_WORKDIR_INSTALL)/boot/grub/grub.cfg
 	$(call process-grub-cfg,$(GRUB_WORKDIR_INSTALL)/boot/grub/grub.cfg)
 	$(call install-grub-theme,$(GRUB_WORKDIR_INSTALL),$(GRUB_WORKDIR_INSTALL)/boot/grub/grub.cfg)
-	$(GRUB_PATH_OVERRIDE) $(GRUB_PREBUILT_DIR)/bin/grub-mkrescue -d $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) --xorriso=$(GRUB_XORRISO_EXEC) -o $(1) $(2) $(GRUB_WORKDIR_INSTALL)
+	$(BOOTMGR_PATH_OVERRIDE) $(GRUB_PREBUILT_DIR)/bin/grub-mkrescue -d $(GRUB_PREBUILT_DIR)/lib/grub/$(TARGET_GRUB_ARCH) --xorriso=$(BOOTMGR_XORRISO_EXEC) -o $(1) $(2) $(GRUB_WORKDIR_INSTALL)
 endef
 
-INSTALLED_ISOIMAGE_INSTALL_TARGET := $(PRODUCT_OUT)/$(GRUB_ARTIFACT_FILENAME_PREFIX).iso
-INSTALLED_ISOIMAGE_INSTALL_TARGET_DEPS := $(PRODUCT_OUT)/kernel $(INSTALLED_COMBINED_RAMDISK_RECOVERY_TARGET) $(PRODUCT_OUT)/$(GRUB_ANDROID_OTA_PACKAGE_NAME)
+INSTALLED_ISOIMAGE_INSTALL_TARGET := $(PRODUCT_OUT)/$(BOOTMGR_ARTIFACT_FILENAME_PREFIX).iso
+INSTALLED_ISOIMAGE_INSTALL_TARGET_DEPS := $(PRODUCT_OUT)/kernel $(INSTALLED_COMBINED_RAMDISK_RECOVERY_TARGET) $(PRODUCT_OUT)/$(BOOTMGR_ANDROID_OTA_PACKAGE_NAME)
 $(INSTALLED_ISOIMAGE_INSTALL_TARGET): $(INSTALLED_ISOIMAGE_INSTALL_TARGET_DEPS)
 	$(call make-isoimage-install-target,$(INSTALLED_ISOIMAGE_INSTALL_TARGET),$(INSTALLED_ISOIMAGE_INSTALL_TARGET_DEPS))
 
