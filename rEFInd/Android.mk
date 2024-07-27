a2c273a1c747e18f66d18681c9531e038575c232
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-28 01:57:24 +0800

    virtio-common: rEFInd: Set LineageOS logo as menuentry icon for lineage builds
    Change-Id: Ic78c404c80ed688349d0414e49422288d0bcee40

--- rEFInd/Android.mk
+++ rEFInd/Android.mk
@@ -40,6 +40,11 @@ define make-espimage
 	cp $(COMMON_REFIND_PATH)/refind-$(4).conf $(3)/fsroot/EFI/BOOT/refind.conf
 	$(call process-bootmgr-cfg-common,$(3)/fsroot/EFI/BOOT/refind.conf)
 
+	$(if $(LINEAGE_BUILD),\
+		cp $(COMMON_REFIND_PATH)/icons/os_lineage.png $(3)/fsroot/EFI/BOOT/icons/ && \
+		sed -i "s|os_linux.png|os_lineage.png|g" $(3)/fsroot/EFI/BOOT/refind.conf \
+	)
+
 	$(call create-espimage,$(1),$(3)/fsroot/EFI $(2),$(4))
 endef
 
