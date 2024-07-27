d9e0e68cc69bd088cd9ab24abf0f5e658038c81c
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-27 20:54:46 +0800

    virtio-common: Use plain ESP image for boot and installation media
    * The initial reason of doing this is, It wasn't possible to build
      ARM64 GRUB image inline, and there's no way to make the kernel
      load ramdisk properly when booting from rEFInd ISO image
    * It turned out that Tianocore EDK II boots plain ESP image just fine
      even if it's on CD-ROM drive... Time to get rid of ISO9660 stuff :D
    
    Change-Id: Ib8bdc497bb49df87cdcfc0f1ad29043781b1eec0

--- rEFInd/Android.mk
+++ rEFInd/Android.mk
@@ -10,7 +10,6 @@ ifeq ($(TARGET_BOOT_MANAGER),rEFInd)
 REFIND_PREBUILT_DIR := prebuilts/rEFInd
 
 REFIND_WORKDIR_BASE := $(TARGET_OUT_INTERMEDIATES)/REFIND_OBJ
-REFIND_WORKDIR_BOOT := $(REFIND_WORKDIR_BASE)/boot
 REFIND_WORKDIR_ESP := $(REFIND_WORKDIR_BASE)/esp
 REFIND_WORKDIR_INSTALL := $(REFIND_WORKDIR_BASE)/install
 
@@ -23,12 +22,25 @@ endif
 # $(1): path to /EFI/BOOT
 define copy-refind-files-to-efi-boot
 	mkdir -p $(1)/drivers_$(REFIND_ARCH)
-	$(foreach drv,ext4 iso9660,\
+	$(foreach drv,ext4,\
 		cp $(REFIND_PREBUILT_DIR)/refind/drivers_$(REFIND_ARCH)/$(drv)_$(REFIND_ARCH).efi $(1)/drivers_$(REFIND_ARCH)/ &&\
 	)true
 	cp $(REFIND_PREBUILT_DIR)/refind/refind_$(REFIND_ARCH).efi $(1)/$(BOOTMGR_EFI_BOOT_FILENAME)
 	cp -r $(REFIND_PREBUILT_DIR)/refind/icons $(1)/
 	cp $(REFIND_PREBUILT_DIR)/LICENSE.txt $(1)/
+endef
+
+# $(1): output file
+# $(2): dependencies
+# $(3): workdir
+# $(4): purpose (boot or install)
+define make-espimage
+	$(call copy-refind-files-to-efi-boot,$(3)/fsroot/EFI/BOOT)
+
+	cp $(COMMON_REFIND_PATH)/refind-$(4).conf $(3)/fsroot/EFI/BOOT/refind.conf
+	$(call process-bootmgr-cfg-common,$(3)/fsroot/EFI/BOOT/refind.conf)
+
+	$(call create-espimage,$(1),$(3)/fsroot/EFI $(2))
 endef
 
 ##### espimage #####
@@ -37,39 +49,16 @@ endef
 # $(2): dependencies
 define make-espimage-target
 	$(call pretty,"Target EFI System Partition image: $(1)")
-	$(call copy-refind-files-to-efi-boot,$(REFIND_WORKDIR_ESP)/fsroot/EFI/BOOT)
-
-	cp $(COMMON_REFIND_PATH)/refind-boot.conf $(REFIND_WORKDIR_ESP)/fsroot/EFI/BOOT/refind.conf
-	$(call process-bootmgr-cfg-common,$(REFIND_WORKDIR_ESP)/fsroot/EFI/BOOT/refind.conf)
-
-	$(call create-espimage,$(1),$(REFIND_WORKDIR_ESP)/fsroot/EFI $(2))
+	$(call make-espimage,$(1),$(2),$(REFIND_WORKDIR_ESP),boot)
 endef
 
-##### isoimage-boot #####
-
-INSTALLED_ISOIMAGE_BOOT_TARGET_EXTRA_DEPS := $(INSTALLED_ESPIMAGE_TARGET)
+##### espimage-install #####
 
 # $(1): output file
 # $(2): dependencies (unused for now)
-define make-isoimage-boot-target
-	$(call pretty,"Target boot ISO image: $(1)")
-	$(call create-isoimage,$(1),$(INSTALLED_ESPIMAGE_TARGET),/EFI.img)
-endef
-
-##### isoimage-install #####
-
-# $(1): output file
-# $(2): dependencies (unused for now)
-define make-isoimage-install-target
-	$(call pretty,"Target installer ISO image: $(1)")
-	$(call copy-refind-files-to-efi-boot,$(REFIND_WORKDIR_INSTALL)/EFI_fsroot/EFI/BOOT)
-
-	cp $(COMMON_REFIND_PATH)/refind-install.conf $(REFIND_WORKDIR_INSTALL)/EFI_fsroot/EFI/BOOT/refind.conf
-	$(call process-bootmgr-cfg-common,$(REFIND_WORKDIR_INSTALL)/EFI_fsroot/EFI/BOOT/refind.conf)
-
-	$(call create-espimage,$(REFIND_WORKDIR_INSTALL)/EFI.img,$(REFIND_WORKDIR_ESP)/EFI_fsroot/EFI $(PRODUCT_OUT)/kernel $(INSTALLED_COMBINED_RAMDISK_RECOVERY_TARGET))
-
-	$(call create-isoimage,$(1),$(PRODUCT_OUT)/$(BOOTMGR_ANDROID_OTA_PACKAGE_NAME) $(REFIND_WORKDIR_INSTALL)/EFI.img,/EFI.img)
+define make-espimage-install-target
+	$(call pretty,"Target installer ESP image: $(1)")
+	$(call make-espimage,$(1),$(2),$(REFIND_WORKDIR_INSTALL),install)
 endef
 
 endif # TARGET_BOOT_MANAGER
