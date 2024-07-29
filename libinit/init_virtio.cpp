4647d18e3872a68eeae6dd76f920f97965e3e25f
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-30 01:22:15 +0800

    virtio-common: libinit: Enable background blur conditionally
    Change-Id: I163a6e8d464128e273e342aa39c9ad4b5ed3aacd

--- libinit/init_virtio.cpp
+++ libinit/init_virtio.cpp
@@ -5,6 +5,8 @@
  */
 
 #include <android-base/file.h>
+#include <android-base/properties.h>
+#include <sys/sysinfo.h>
 
 #include <libinit_dalvik_heap.h>
 #include <libinit_utils.h>
@@ -13,6 +15,9 @@
 
 #include <unordered_map>
 
+#define GB(b) (b * 1024ull * 1024 * 1024)
+
+using android::base::GetProperty;
 using android::base::ReadFileToString;
 
 static const std::string kDmiIdPath = "/sys/devices/virtual/dmi/id/";
@@ -27,6 +32,18 @@ static const std::unordered_map<std::string, std::string> kDmiIdToRoBuildPropMap
     {"product_name", "model"},
     {"sys_vendor", "manufacturer"},
 };
+
+static void set_misc_properties() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram > GB(4)) {
+        // Consider as high-performance
+        if (GetProperty("ro.boot.graphics", "") != "swiftshader") {
+            property_override("ro.surface_flinger.supports_background_blur", "1");
+        }
+    }
+}
 
 static void set_properties_from_dmi_id() {
     std::string value;
@@ -50,5 +67,6 @@ static void set_properties_from_dmi_id() {
 
 void vendor_load_properties() {
     set_dalvik_heap();
+    set_misc_properties();
     set_properties_from_dmi_id();
 }
