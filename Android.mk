7ca31e34f170989fafe15371cd7e86d5be124934
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-02 20:40:38 +0800

    virtio-common: Add build rules to create prebuilt kernel repo
    Change-Id: If7664c4dba304a6b244bcafa79cdd370f387b68d

--- Android.mk
+++ Android.mk
@@ -196,4 +196,27 @@ diskimage-vda-nodeps:
 	@echo "make $(INSTALLED_DISKIMAGE_VDA_TARGET): ignoring dependencies"
 	$(call make-diskimage-target,$(INSTALLED_DISKIMAGE_VDA_TARGET),vda)
 
+# Create prebuilt kernel repo
+
+ifneq ($(LINEAGE_BUILD),)
+ifneq ($(wildcard $(TARGET_KERNEL_SOURCE)/Makefile),)
+
+INSTALLED_PREBUILT_KERNEL_REPO_DIR := out/kernel-virtio/$(TARGET_PREBUILT_KERNEL_USE)/$(TARGET_PREBUILT_KERNEL_ARCH)
+
+INSTALLED_PREBUILT_KERNEL_REPO_KERNEL_TARGET := $(INSTALLED_PREBUILT_KERNEL_REPO_DIR)/kernel
+$(INSTALLED_PREBUILT_KERNEL_REPO_KERNEL_TARGET): $(PRODUCT_OUT)/kernel
+	$(call pretty,"Target prebuilt kernel repo: $@")
+	mkdir -p $(INSTALLED_PREBUILT_KERNEL_REPO_DIR)
+	rm -f $(INSTALLED_PREBUILT_KERNEL_REPO_DIR)/*.ko
+	if grep -q '=m' $(PRODUCT_OUT)/obj/KERNEL_OBJ/.config; then\
+		cp `find $(PRODUCT_OUT)/obj/KERNEL_OBJ/ -type f -name "*.ko"` $(INSTALLED_PREBUILT_KERNEL_REPO_DIR)/;\
+	fi
+	cp $(PRODUCT_OUT)/kernel $@
+
+.PHONY: prebuilt-kernel-repo
+prebuilt-kernel-repo: $(INSTALLED_PREBUILT_KERNEL_REPO_KERNEL_TARGET)
+
+endif # $(TARGET_KERNEL_SOURCE)/Makefile
+endif # LINEAGE_BUILD
+
 endif
