62c89bcac4bfefce3008a7fe1dcc7225b2fa763d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-03 00:27:11 +0800

    virtio-common: fastboot: AIDL => HIDL
    Change-Id: I44eef2e7b6121498acde38d29858059250dee7c9

--- fastboot/Fastboot.h
+++ fastboot/Fastboot.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,28 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #pragma once
 
-#include "aidl/android/hardware/fastboot/BnFastboot.h"
+#include <android/hardware/fastboot/1.1/IFastboot.h>
+#include <hidl/Status.h>
 
-namespace aidl {
 namespace android {
 namespace hardware {
 namespace fastboot {
-class Fastboot : public BnFastboot {
-    ::ndk::ScopedAStatus doOemCommand(const std::string& in_oemCmd,
-                                      std::string* _aidl_return) override;
-    ::ndk::ScopedAStatus doOemSpecificErase() override;
-    ::ndk::ScopedAStatus getBatteryVoltageFlashingThreshold(int32_t* _aidl_return) override;
-    ::ndk::ScopedAStatus getOffModeChargeState(bool* _aidl_return) override;
-    ::ndk::ScopedAStatus getPartitionType(
-            const std::string& in_partitionName,
-            ::aidl::android::hardware::fastboot::FileSystemType* _aidl_return) override;
-    ::ndk::ScopedAStatus getVariant(std::string* _aidl_return) override;
+namespace V1_1 {
+namespace implementation {
+
+using ::android::hardware::hidl_string;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::fastboot::V1_0::FileSystemType;
+using ::android::hardware::fastboot::V1_0::Status;
+using ::android::hardware::fastboot::V1_0::Result;
+
+struct Fastboot : public IFastboot {
+    // Methods from ::android::hardware::fastboot::V1_0::IFastboot follow.
+    Return<void> getPartitionType(const hidl_string& partitionName,
+                                  getPartitionType_cb _hidl_cb) override;
+    Return<void> doOemCommand(const hidl_string& oemCmd, doOemCommand_cb _hidl_cb) override;
+    Return<void> getVariant(getVariant_cb _hidl_cb) override;
+    Return<void> getOffModeChargeState(getOffModeChargeState_cb _hidl_cb) override;
+    Return<void> getBatteryVoltageFlashingThreshold(
+            getBatteryVoltageFlashingThreshold_cb _hidl_cb) override;
+    Return<void> doOemSpecificErase(doOemSpecificErase_cb _hidl_cb) override;
 };
 
+extern "C" IFastboot* HIDL_FETCH_IFastboot(const char* name);
+
+}  // namespace implementation
+}  // namespace V1_1
 }  // namespace fastboot
 }  // namespace hardware
 }  // namespace android
-}  // namespace aidl
