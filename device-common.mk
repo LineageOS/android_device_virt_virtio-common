7176de082f8f906b100284a59e9216397b8565ae
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-06 06:41:55 +0800

    virtio-common: Build Memtrack HAL APEX
    Change-Id: I29bcc719b6361ca11170881c22cccc841cf8cd04

--- device-common.mk
+++ device-common.mk
@@ -156,6 +156,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     LowPerformanceSettingsProviderOverlay
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    com.android.hardware.memtrack
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
