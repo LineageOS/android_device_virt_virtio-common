16f09615b8c97a05ea25c4c69fb914f0e777ac27
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-11 15:20:13 +0800

    virtio-common: Switch to drm HWC AIDL HAL APEX
    Change-Id: I3e14e45a8307e2ece0aa7ccf6fc1a8f4adfcebb9

--- device-common.mk
+++ device-common.mk
@@ -11,11 +11,7 @@ COMMON_PATH := device/virt/virtio-common
 
 # Graphics (Composer)
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-service \
-    hwcomposer.drm
-
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.hardware.hwcomposer=drm
+    com.android.hardware.graphics.composer.drm_hwcomposer
 
 # Graphics (Gralloc)
 PRODUCT_PACKAGES += \
