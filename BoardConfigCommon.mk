32e0cd19da2f13ba93ef66699e6cec8468be0c30
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-06 02:01:11 +0800

    virtio-common: Move kernel config `virtio.config` to target device tree
    Change-Id: I36aea9053b81b388022168a3c8a40503e6619e45

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,8 +36,7 @@ BOARD_KERNEL_CMDLINE += \
     console=tty0
 
 ifneq ($(wildcard $(TARGET_KERNEL_SOURCE)/Makefile),)
-TARGET_KERNEL_CONFIG += \
-    lineageos/virtio.config
+# nothing
 else ifneq ($(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/kernel),)
 BOARD_VENDOR_KERNEL_MODULES := \
     $(wildcard $(TARGET_PREBUILT_KERNEL_DIR)/*.ko)
