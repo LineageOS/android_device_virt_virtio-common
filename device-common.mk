62d88136cfc2122d9d43def58fd41ee321878c03
Yumi Yukimura <me.cafebabe@gmail.com>
2026-03-05 16:02:57 +0800

    [DNM] try tinyhal
    Change-Id: Iab22cfbbccfd13478f72088979f0a4d4a9c98ed0

--- device-common.mk
+++ device-common.mk
@@ -8,6 +8,12 @@ $(call inherit-product, device/virt/virt-common/virt-common.mk)
 
 COMMON_PATH := device/virt/virtio-common
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/audio/audio.virtio_x86_64.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio.virtio_x86_64.xml
+
+TARGET_AUDIO_HAL := tinyhal
+
 # Graphics
 PRODUCT_PACKAGES += \
     virtgpu_detect
@@ -48,11 +54,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.recovery.virtio.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.virtio.rc
 
 # Shipping API level
-ifeq ($(TARGET_AUDIO_HAL),ranchu)
 PRODUCT_SHIPPING_API_LEVEL := 34
-else
-PRODUCT_SHIPPING_API_LEVEL := 35
-endif
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
