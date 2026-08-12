c1a8268ce79aad02b0cfc7a380770754f0f66b27
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-12 19:47:52 +0800

    virtio-common: Bump target-level to 202604
    Change-Id: Ibe89bd0273f1c8fc83e2ee98b642d0ad63a95ad6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,7 +72,7 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 # VINTF
 ifeq ($(TARGET_AUDIO_HAL),ranchu)
 DEVICE_MANIFEST_FILE += \
-    $(COMMON_PATH)/configs/vintf/manifest_target-level-8.xml
+    $(COMMON_PATH)/configs/vintf/manifest_target-level-latest.xml
 else
 DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/configs/vintf/manifest_target-level-202404.xml
